f612528c36b3d2a82ab7bd93704e6a43fd0c53c1
Karassay <karassayraushanbek@gmail.com>
2023-11-18 18:06:05 +0600

    Init

--- backend/src/auth/router.py
+++ backend/src/auth/router.py
@@ -6,12 +6,22 @@ from src.auth.service import authenticate_user, create_access_token
 from src.auth.models import Token
 from src.config import AppSettings
 
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
 router = APIRouter()
 settings = AppSettings()
 
 @router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
