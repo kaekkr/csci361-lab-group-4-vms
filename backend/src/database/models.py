4c0457d958c9cb57d0669003952643327a2141ce
Karassay <karassayraushanbek@gmail.com>
2023-11-20 20:55:32 +0600

    refactoring

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,17 +1,6 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 
 from src.database.database import Base
-
-
-# class User(Base):
-#     __tablename__ = "users"
-
-#     id = Column(Integer, primary_key=True, index=True)
-#     username = Column(String, unique=True, index=True)
-#     email = Column(String, unique=True, index=True)
-#     full_name = Column(String, nullable=True)
-#     password = Column(String)
-#     disabled = Column(Boolean, nullable=True)
 
 
 class Admin(Base):
